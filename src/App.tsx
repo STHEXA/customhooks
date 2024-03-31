d8eddb36c9576af09f8b563a3f7c0863385232d9
STHEXA <ti.syu.s@gmail.com>
2024-03-31 22:36:04 +0900

    カスタムフックを使ってみる

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,24 @@
 import { UserCard } from './components/UserCard';
-import axios from 'axios';
-import { User } from './types/api/user';
-import { useState } from 'react';
-import { UserProfile } from './types/userProfile';
-
-const user = {
-  id: 1,
-  name: 'rokuroku',
-  email: 'test@gmail.com',
-  address: 'address',
-};
+import { useAllUsers } from './hooks/useAllUsers';
 
 function App() {
-  const [userProfiles, setuserProfiles] = useState<Array<UserProfile>>([]);
-  const onClickFetchUser = () => {
-    axios
-      .get<Array<User>>('https://jsonplaceholder.typicode.com/users')
-      .then((res) => {
-        const data = res.data.map((user) => ({
-          id: user.id,
-          name: `${user.name}(${user.username})`,
-          email: user.email,
-          address: `${user.address.city}${user.address.suite}${user.address.street}`,
-        }));
-        setuserProfiles(data);
-      });
-  };
+  const {getUsers, userProfiles, loading, error} = useAllUsers();
+  const onClickFetchUser = () => getUsers();
   return (
     <>
       <button onClick={onClickFetchUser}>データ取得</button>
-      {userProfiles.map((user) => (
-        <UserCard key={user.id} user={user} />
-      ))}
+      <br />
+      {error ? (
+        <p　style={{color: "red"}}>データの取得に失敗しました</p>
+      ) : loading ? (
+        <p>loading...</p>
+      ) : (
+        <>
+        {userProfiles.map((user) => (
+          <UserCard key={user.id} user={user} />
+        ))}
+        </>
+      )}
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App.tsx';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootNode = document.getElementById('root');
+ReactDOM.render(<App />, rootNode);
